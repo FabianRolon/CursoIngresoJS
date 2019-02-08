f507e30ed70d537c2219f535f5979253809df9dc
Skoivan <fabi18fr@gmail.com>
2019-02-08 17:17:36 -0300

    Ejercicio 3 del parcial resuelto

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var preciofinal;
+
+	precio = prompt("Ingrese el precio:");
+	descuento = prompt("Ingrese el descuento");
+
+	preciofinal = precio * (1-descuento/100);
+
+	elPrecioFinal.value = preciofinal;
+
+
 
 }
