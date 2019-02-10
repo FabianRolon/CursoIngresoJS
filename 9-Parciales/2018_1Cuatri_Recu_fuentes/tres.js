2c658fc77e84efe5bb521681a80588bead9b2037
Skoivan <fabi18fr@gmail.com>
2019-02-10 18:50:41 -0300

    Ejercicio 3 de parcial corregido

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -7,7 +7,7 @@ function mostrar()
 	precio = prompt("Ingrese el precio");
 	porcentaje = prompt("Ingrese el porcentaje de descuento");	
 
-	preciofinal = precio * (1-porcentaje);
+	preciofinal = precio * ( 1 - porcentaje / 100);
 
 	elPrecioFinal.value = preciofinal;
 }
