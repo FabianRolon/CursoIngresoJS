e4649dc7d397a9242bd23d11b550ebda001adc34
alumno <alumno>
2019-02-11 20:21:52 -0300

    ejercicio 3 y 4 de if resuelto

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,19 @@
 function mostrar()
 {
-//tomo la edad  
+	//tomo la edad  
+	var laEdad;
 
+	laEdad = edad.value;
+
+	if (laEdad > 17)
+		{
+			alert("La persona es mayor de edad");
+		}
+
+
+	else
+		{
+			alert("La persona es menor de edad");
+		}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,15 @@
 function mostrar()
 {
-//tomo la edad  
+	//tomo la edad  
+	var laEdad;
 
+	laEdad = edad.value;
+
+	if (laEdad > 12 && laEdad < 18)
+	{
+		alert("La persona es adolescente");
+	}
+			
 
 
 }//FIN DE LA FUNCIÓN
