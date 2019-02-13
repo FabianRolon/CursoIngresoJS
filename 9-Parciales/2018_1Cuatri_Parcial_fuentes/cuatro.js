9f68df135a77a2b01e9685724de867e410424877
alumno <alumno>
2019-02-13 20:09:26 -0300

    ejercicio 3 de parcial resuelto

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,41 +1,44 @@
+/*al realizar una compra, si compra de mas e dos productos se realiza un descuento del 10%
+y si supera los $2000 se agrega un descuento adicional de 15%, si el pago es con tarjeta y no efectivo
+al precio final se le agrega un 10% de recargo*/
+
 function mostrar()
 {
-	var primerNumero;
-	var segundoNumero;
-	var suma;
+	var cantidadCompra;
+	var precioTotalCompra;
+	var recargo;
+	var metodoDePago;
 
-	primerNumero = prompt("Ingrese el primer numero");
-	segundoNumero = prompt("Ingrese el segundo numero");
+	cantidadCompra = prompt("Cuantos productos compra?");
+	precioTotalCompra = prompt("Cual es el valor total de la compra?")
+	metodoDePago = prompt("Si paga en efectivo ingrese 1, sino 0");
+
+	metodoDePago = parseInt(metodoDePago);
+	precioTotalCompra = parseInt(precioTotalCompra);
+	cantidadCompra = parseInt(cantidadCompra);
+
+	if(cantidadCompra > 2)	
+	{
+		precioTotalCompra = precioTotalCompra * (1 - 1/10);
+		
+		if(precioTotalCompra > 2000)
+		{
+			precioTotalCompra = precioTotalCompra * (1 - 15/100);
+		}
+		
+	}
+	
+	
+	if(metodoDePago == 0)
+	{
+			precioTotalCompra = precioTotalCompra * (1 + 1/10);
+	}
+
+	alert("El precio final de su compra es: "+precioTotalCompra);
+
 
 	
 
-	if(primerNumero == segundoNumero)
-	{
-		alert(primerNumero+segundoNumero);
-	}
-
-	primerNumero = parseInt(primerNumero);
-	segundoNumero = parseInt(segundoNumero);
-
-	if(primerNumero > segundoNumero)
-	{
-		suma = primerNumero - segundoNumero;
-		alert("La resta es: "+suma);
-	}
-	if(primerNumero < segundoNumero)
-	{
-		suma = primerNumero + segundoNumero;
-		
-		if(suma > 10)
-		{
-			alert("La suma es "+suma+" y supero el 10");
-		}
-		else 
-		{
-			alert("La suma es: "+suma);
-		}
-		
-		
-		
-	}
+	
+	
 }
