7369c149403ae9245815d31e3270ff33e87aabe7
Skoivan <fabi18fr@gmail.com>
2019-02-13 10:09:43 -0300

    Ejercicio 10 de IF resuelto

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,24 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var nota;
 	
+	nota = Math.floor((Math.random() * 10) + 1);
 
+	if (nota < 4 || nota > 8)
+	{
+		if(nota < 4)
+		{
+			alert("La nota es: "+nota+" Vamos, la proxima se puede");
+		}
+
+		if(nota > 8)
+		{
+			alert("La nota es: "+nota+" Excelente");
+		}
+	}
+	else 
+	{
+		alert("La nota es: "+nota+" Aprobó");
+	}
 }//FIN DE LA FUNCIÓN
