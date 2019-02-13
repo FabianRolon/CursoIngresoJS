6dded958f7d3f13921c2c17563ef7f02f94e4dd8
Skoivan <fabi18fr@gmail.com>
2019-02-13 09:31:40 -0300

    Ejercicio 8 de IF resuelto

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -7,19 +7,19 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 function Rectangulo () 
 {
 
-var longitud;
-var altura;
-var perimetroalambre;
+	var longitud;
+	var altura;
+	var perimetroalambre;
 
-longitud = Largo.value;
-altura = Ancho.value;
+	longitud = Largo.value;
+	altura = Ancho.value;
 
-longitud = parseInt(longitud);
-altura = parseInt(altura);
+	longitud = parseInt(longitud);
+	altura = parseInt(altura);
 
-perimetroalambre = (longitud + altura)*3;
+	perimetroalambre = (longitud + altura)*3;
 
-alert("Se necesitan "+perimetroalambre+" metros de alambre");
+	alert("Se necesitan "+perimetroalambre+" metros de alambre");
 
 }
 function Circulo () 
@@ -42,14 +42,14 @@ function Materiales ()
 	var cemento;
 	var cal;
 
-longitud = Largo.value;
-altura = Ancho.value;
+	longitud = Largo.value;
+	altura = Ancho.value;
 
-area = (longitud * altura);
-cemento = 2*(area);
-cal = 3*(area);
+	area = (longitud * altura);
+	cemento = 2*(area);
+	cal = 3*(area);
 
 
-alert("Se necesitan "+cemento+" bolsas de cemento y "+cal+" bolsas de cal");
+	alert("Se necesitan "+cemento+" bolsas de cemento y "+cal+" bolsas de cal");
 
 }
