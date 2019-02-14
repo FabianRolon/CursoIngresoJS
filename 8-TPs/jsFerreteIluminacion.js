cce4325204eaa324433579c9fe93c4e3562febe0
Skoivan <fabi18fr@gmail.com>
2019-02-14 16:49:59 -0300

    8-TPs/jsFerreteIluminacion.js TERMINADO

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,80 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
+ 	var cantidad;
+ 	var marca;
+ 	var precioFinal;
+ 	var precioLamparita = 35;
+ 	var ingresosBrutos;
+
+ 	cantidad = Cantidad.value;
+ 	marca = Marca.value;
+
+ 	cantidad = parseInt(cantidad);
+
+ 	if(cantidad < 3)
+ 	{
+ 		precioFinal = cantidad*precioLamparita;
+ 	}
+
+ 	if(cantidad > 5)
+ 	{
+ 		precioFinal = cantidad*precioLamparita/2;
+ 	}
+
+ 	if(cantidad == 5)
+ 	{
+ 		if(marca == "ArgentinaLuz")
+ 		{
+ 			precioFinal = cantidad*precioLamparita;
+ 			precioFinal = precioFinal*0.6;
+ 		}else
+ 		{
+ 			precioFinal = cantidad*precioLamparita;
+ 			precioFinal = precioFinal*0.7;
+ 		}
+ 	}
+
+ 	if(cantidad == 4)
+ 	{
+ 		if(marca == "ArgentinaLuz" || marca == "FelipeLamparas")
+ 		{
+ 			precioFinal = cantidad*precioLamparita;
+ 			precioFinal = precioFinal*0.75;
+ 		}else
+ 		{
+ 			precioFinal = cantidad*precioLamparita;
+ 			precioFinal = precioFinal*0.8;
+ 		}
+ 	}
+
+ 	if(cantidad == 3)
+ 	{
+ 		if(marca == "ArgentinaLuz")
+ 		{
+ 			precioFinal = cantidad*precioLamparita;
+ 			precioFinal = precioFinal*0.85;
+ 		}
+ 		if(marca == "FelipeLamparas")
+ 		{
+ 			precioFinal = cantidad*precioLamparita;
+ 			precioFinal = precioFinal*0.9;
+ 		}
+ 		if(marca != "ArgentinaLuz" && marca != "FelipeLamparas")
+ 		{
+ 			precioFinal = cantidad*precioLamparita;
+ 			precioFinal = precioFinal*0.95;
+ 		}
+ 	}
+
+ 	if(precioFinal > 120)
+ 	{
+ 		ingresosBrutos = precioFinal/10;
+ 		precioFinal = precioFinal + ingresosBrutos;
+ 		
+ 		alert("Usted pago $"+ingresosBrutos+" de IIBB");
+ 	}
+ 	
+ 	precioDescuento.value = "$"+precioFinal;
  	
 }
