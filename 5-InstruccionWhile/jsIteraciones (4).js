90ee5f7f93e18e96b5f2b0ac1188070b78d9aa75
Skoivan <fabi18fr@gmail.com>
2019-02-20 15:56:32 -0300

    Realizados ejercicios 2, 3 y 4 de iteraciones while

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -3,5 +3,11 @@ function mostrar()
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
 
+	while(numero > 10)
+	{
+		numero = prompt("ingrese un número entre 0 y 10.");
+	}
+
+	Numero.value = "El numero es "+numero+" y es correcto";
 
 }//FIN DE LA FUNCIÓN
