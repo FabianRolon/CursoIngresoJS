4b02a3ee9338d5284b47a18907a75950e0b4f0e1
alumno <alumno>
2019-02-20 22:14:25 -0300

    ejercicios 8 while resuelto

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,8 +4,29 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
 	var respuesta='si';
+	var numero;
+	
+	
+	
+	while(respuesta == "si")
+	{
+		contador++;
+		numero = prompt("Ingrese el numero "+contador);
+		numero = parseInt(numero);
+		
+		if(numero<0)
+		{
+			negativo = negativo*numero;
+		}
+		else
+		{
+			positivo = positivo+numero;
+		}
+
+		respuesta = prompt("Desea ingresar otro numero? si para continuar, sino presone cualquier tecla")
+		
+	}
 
 
 document.getElementById('suma').value=positivo;
