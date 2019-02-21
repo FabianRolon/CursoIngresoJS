5057c5c9ca52963eabec3929efb2886523c3bd99
alumno <alumno>
2019-02-20 21:38:42 -0300

    ejercicio 5 while terminado

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -4,7 +4,11 @@ function mostrar()
 var sexo = prompt("ingrese f ó m .");
 
 
+while(sexo != "f" && sexo != "m")
+{
+	sexo = prompt("ingrese f ó m .");
+}
 
-document.getElementById('Sexo').value=sexo;
+document.getElementById('Sexo').value = "El sexo es "+sexo+" y es correcto";
 
 }//FIN DE LA FUNCIÓN
