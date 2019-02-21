6450a6cb1063deee1005f83dcefe0437764f6909
alumno <alumno>
2019-02-20 22:04:56 -0300

    ejercicios 6 y 7 while resueltos bein

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -2,19 +2,20 @@ function mostrar()
 {
 
 	var contador = 0;
-	var acumulador;
-	var Suma = 0;
+	var acumulador = 0;
+	var numero;
 
 	while(contador < 5)
 	{
-		acumulador = prompt(acumulador);
-		acumulador = parseInt(acumulador);
-		Suma = Suma + acumulador;
 		contador++;
+		numero = prompt("Ingrese el numero "+contador);
+		numero = parseInt(numero);
+		acumulador = numero + acumulador;
+		
 	}
 acumulador = Suma;
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById('promedio').value=acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,14 +4,18 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta="si";
-	var Suma;
-
-	while(respuesta != "si")
+	var numero;
+	
+	
+	
+	while(respuesta == "si")
 	{
-		acumulador = prompt("Ingrese un numero");
-		acumulador = parseInt(acumulador);
-		Suma = Suma + acumulador;
-		respuesta = prompt("Desea ingresar otro numero? si para continuar, sino presone c")
+		contador++;
+		numero = prompt("Ingrese el numero "+contador);
+		numero = parseInt(numero);
+		acumulador = numero + acumulador;
+		respuesta = prompt("Desea ingresar otro numero? si para continuar, sino presone cualquier tecla")
+		
 	}
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
