7b0949e1c2bb9a1c0460bba2a97ddce6fefa2e0d
Skoivan <fabi18fr@gmail.com>
2019-02-24 17:23:28 -0300

    Ej 9 while resuelto

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,17 +2,40 @@ function mostrar()
 {
 
 	var contador=0;
-	// declarar variables
-	
+	var numero;
+	var Maximo;
+	var Minimo;	
 	var respuesta='si';
 
-	while(respuesta!='no')
+	while(respuesta == "si")
 	{
+		contador++;
+		numero = prompt("Ingrese el numero "+contador);
+		numero = parseInt(numero);
 		
-	
+		if(contador == 1)
+		{
+			Maximo = numero;
+			Minimo = numero;
+		}
+
+		if(numero>Maximo)
+		{
+			Maximo = numero;
+		}
+		else
+		{
+			if(numero<Minimo)
+			{
+				Minimo = numero;
+			}
+		}
+
+		respuesta = prompt("Desea ingresar otro numero? si para continuar, sino presone cualquier tecla")
+		
 	}
 
-
-
+	maximo.value = Maximo;
+	minimo.value = Minimo;
 
 }//FIN DE LA FUNCIÓN
