1550aeebb3672494b7dfb8bcde69b6e9a5a50aa0
Skoivan <fabi18fr@gmail.com>
2019-02-27 00:11:06 -0300

    ejercicio nueve del recuperatorio resuelto

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,93 @@
 function mostrar()
 {
+	var peso;
+	var validador='si';
+	var contador=0;
+	var nombreAnimal;
+	var temperaturasPar=0;
+	var contadorAnimalbajoCero=0;
+	var pesoTotal=0;
+	var promedioPeso;
+	var pesoMaximo = 1;
+	var pesoMinimo = 1000;
+	var nombreAnimalMaximo;
+	var nombreAnimalMinimo;
+	var temperaturaMasPesado;
+	var pesoMaximoBajoCero = 1;
+	var pesoMinimoBajoCero = 1000;
 
+	while(validador == 'si')
+ 	{
+		contador++;
+
+		nombreAnimal = prompt("Ingrese el nombre del animal" );
+		
+
+		while(!(isNaN(nombreAnimal)) || nombreAnimal.length == 0)
+		{
+			nombreAnimal= prompt("ERROR! Ingrese el nombre del animal");
+		}
+
+		peso = prompt("Ingrese el peso entre 1 y 1000");
+		peso = parseInt(peso);
+
+		while(isNaN(peso) || peso > 1000 || peso < 1)
+		{
+			peso = prompt("ERROR! Ingrese el peso entre 1 y 1000");
+			peso = parseInt(peso);
+		}
+
+		temperatura = prompt("Ingrese la temperatura del habitad entre -30 y 30");
+		temperatura = parseInt(temperatura);
+
+		while(isNaN(temperatura) || temperatura > 30 || temperatura < -30)
+		{
+			temperatura = prompt("ERROR! Ingrese el temperatura entre -30 y 30");
+			temperatura = parseInt(temperatura);
+		}
+
+		pesoTotal = pesoTotal + peso;
+
+		if(peso > pesoMaximo)
+		{
+			pesoMaximo = peso;
+			nombreAnimalMaximo = nombreAnimal;
+			temperaturaMasPesado = temperatura;
+		}
+
+	
+		if(temperatura%2 == 0)
+		{
+			temperaturasPar++;
+		}
+
+		if(temperatura < 0)
+		{
+			contadorAnimalbajoCero++;
+			
+			if(peso > pesoMaximoBajoCero)
+			{
+				pesoMaximoBajoCero = peso;
+			}
+			else
+			{
+				if(peso < pesoMinimoBajoCero)
+				{
+					pesoMinimoBajoCero = peso;
+				}
+				
+			}
+
+		}
+
+		promedioPeso = pesoTotal/contador;
+
+		validador = prompt("Desea continuar 'si' o 'presione cualquier tecla/s' para salir");
+	}	
+
+	alert("La cantidad de temperaturas pares es: "+temperaturasPar);
+	alert("El nombre y temperatura del animal mas pesado es: "+nombreAnimalMaximo+" "+temperaturaMasPesado+"°C");
+	alert("La cantidad de animales que viven bajo cero es: "+contadorAnimalbajoCero);
+	alert("El promedio del peso de todos los animales es: "+promedioPeso);
+	alert("El peso maximo y minimo de los animales que viven bajo cero es: "+pesoMaximoBajoCero+ " y "+pesoMinimoBajoCero);
 }
