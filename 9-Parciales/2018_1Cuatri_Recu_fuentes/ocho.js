06a282e3aff15b83fcded89f2ee7e5ffb2b987af
Skoivan <fabi18fr@gmail.com>
2019-02-26 23:18:49 -0300

    ejercicio ocho del recuperatorio resuelto

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -2,11 +2,23 @@ function mostrar()
 {
 	var numero;
 	var validador='si';
-	var contador;
+	var contador=0;
 	var letra;
-	
+	var contadorPar=0;
+	var contadorImpar=0;
+	var sumaPositivos=0;
+	var contadorCeros=0;
+	var sumaNegativos=0;
+	var promedioPositivos;
+	var maximo = -100;
+	var minimo =  100;
+	var letraMaximo;
+	var letraMinimo;
+
 	while(validador == 'si')
  	{
+		contador++;
+
 		letra = prompt("Ingrese una letra" );
 		
 
@@ -24,10 +36,54 @@ function mostrar()
 			numero = parseInt(numero);
 		}
 
+		if (numero == 0)
+		{
+			contadorCeros++;
+		}
+		else
+		{
+			if(numero < minimo)
+			{
+				minimo = numero;
+				letraMinimo = letra;
+			}
+			else
+			{
+				if(numero > maximo)
+				{
+					maximo = numero;
+					letraMaximo = letra;
+				}
+			}
+		}
+		
+		if(numero%2 == 0)
+		{
+			contadorPar++;
+		}
+		else
+		{
+			contadorImpar++;
+		}
+
+		if(numero > 0)
+		{
+			sumaPositivos = sumaPositivos + numero;
+		}
+		else
+		{
+			sumaNegativos = sumaNegativos + numero;
+		}
+
+		promedioPositivos = sumaPositivos/contador;
+
 		validador = prompt("Desea continuar 'si' o 'no'");
-
-
-
 	}	
-	
+
+	alert("La cantidad de numeros pares es: "+contadorPar);
+	alert("La cantidad de numeros impares es: "+contadorImpar);
+	alert("La cantidad de CEROS es: "+contadorCeros);
+	alert("El promedio de los numeros positivos es: "+promedioPositivos);
+	alert("La suma de los numeros negativos es: "+sumaNegativos);
+	alert("El numero y la letra del maximo es: "+letraMaximo+maximo+" El numero y la letra del minimo es: "+letraMinimo+minimo);
 }
